1efa58ba3ce7a211df5de408953ac7d3a62f2aa5
Toky <herostoky@gmail.com>
2025-05-31 18:25:50 +0300

    Added Avalonia Project

---
+++ Src/MergeClone/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MergeClone
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
